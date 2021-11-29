3b94682a0e74c3d822a9df09f5f3b7eda0e7af30
FlipeFrontDev <felipe.saint@gmail.com>
2021-11-29 11:01:39 -0300

    Update Readme

--- Avaliacoes/Test/Checkpoint2/src/main/java/com/example/checkpoint_clinica/persistence/repository/IAddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.checkpoint_clinica.persistence.repository;
-
-import com.example.checkpoint_clinica.persistence.entities.AddressEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//Set JpaRepository queries provider
-public interface IAddressRepository extends JpaRepository<AddressEntity, Integer> {
-
-}
--- Avaliacoes/Test/Checkpoint2/src/main/java/com/example/checkpoint_clinica/persistence/repository/IDentistRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.checkpoint_clinica.persistence.repository;
-
-import com.example.checkpoint_clinica.persistence.entities.DentistEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//Set JpaRepository queries provider
-public interface IDentistRepository extends JpaRepository<DentistEntity, Integer> {
-
-}
